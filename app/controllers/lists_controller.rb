2665103cb2771e7e35edd54a428852bffad5b46a
worace <horace.d.williams@gmail.com>
2015-05-05 17:53:07 -0600

    create a list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,4 +5,19 @@ class ListsController < ApplicationController
   def new
     @list = List.new
   end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title)
+  end
 end
