3051eeafef468de26c579668203da6be1d7c5454
worace <horace.d.williams@gmail.com>
2015-05-05 17:55:57 -0600

    dont show archived lists

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,3 @@
 class List < ActiveRecord::Base
+  default_scope { where(archived: false) }
 end
--- test/integration/viewing_lists_test.rb
+++ test/integration/viewing_lists_test.rb
@@ -8,4 +8,18 @@ class ViewingListsTest < ActionDispatch::IntegrationTest
     assert_equal root_path, current_path
     assert_equal "My List", List.last.title
   end
+
+  def test_it_renders_a_list
+    List.create(title: "My List")
+    visit root_path
+    assert page.has_content?("My List")
+  end
+
+  def test_it_doesnt_show_archived_lists
+    List.create(title: "My List")
+    List.create(title: "Archived", archived: true)
+    visit root_path
+    assert page.has_content?("My List")
+    refute page.has_content?("Archived")
+  end
 end
